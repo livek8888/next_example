9cc0ef9b0d6a0811a19bf353054e35742714679c
kkam678 <kkam678@gmail.com>
2021-11-18 08:29:12 +0900

    typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,14 +4,14 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'MS'
-export const siteTitle = 'Next.js Sample Website'
+const name: string = 'MS'
+export const siteTitle: string = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({children, home}: { children: React.ReactNode, home?: boolean }) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
                 <meta
                     name="description"
                     content="Learn how to build a personal website using Next.js"
@@ -22,8 +22,8 @@ export default function Layout({ children, home }) {
                         siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
             </Head>
             <header className={styles.header}>
                 {home ? (
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,9 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../components/date'
 import {getSortedPostsData} from '../lib/posts'
+import {GetStaticProps} from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             {/* Keep the existing code here */}
@@ -15,14 +16,14 @@ export default function Home({ allPostsData }) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
+                    {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/posts/${id}`}>
                                 <a>{title}</a>
                             </Link>
-                            <br />
+                            <br/>
                             <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
+                                <Date dateString={date}/>
                             </small>
                         </li>
                     ))}
@@ -32,7 +33,7 @@ export default function Home({ allPostsData }) {
     )
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(): Promise<object> {
     const allPostsData = getSortedPostsData()
     return {
         props: {
