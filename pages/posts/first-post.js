db68c5110c758e439c97511b32ed30fca74e43c5
kkam678 <kkam678@gmail.com>
2021-11-18 08:14:53 +0900

    commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+            <Image
+                src="/images/profile.jpg" // Route of the image file
+                height={800} // Desired size with correct aspect ratio
+                width={800} // Desired size with correct aspect ratio
+                alt="Your Name"
+            />
+        </Layout>
+    )
+}
